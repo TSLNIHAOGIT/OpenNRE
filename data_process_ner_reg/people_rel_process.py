f47414976f145f18e70271627aad2da7dcd2048d
taoshilai <taoshilai@x3platform.com>
2019-04-29 14:19:25 +0800

    update readme

--- data_process_ner_reg/people_rel_process.py
+++ data_process_ner_reg/people_rel_process.py
@@ -1,5 +1,6 @@
 
 import json
+import pickle
 # df=pd.read_csv('../data/train_people_relation.txt',delimiter='\t')
 # print(df['rel'].value_counts())
 
@@ -29,10 +30,19 @@ with open('../data/sgns.baidubaike.bigram-char',encoding='utf8') as f:
         else:
             word_dict['word']=each[0]
             word_dict['vec']=each[1:]
-            word_vec_new.append(word_dict)
+            # word_vec_new.append(word_dict)
+            if index%50000==0:
+                print('index={},word_dict={}'.format(index,word_dict))
+print('len word_vec_new',len(word_vec_new))
 print('开始保存文件')
-with open('../data/word_vec_new.json','w',encoding='utf8') as f:
-    #两种效果一样
-    ## f.write(json.dumps(all_rel_data,indent=4))
-    json.dump(word_vec_new,f,ensure_ascii=False)
+# with open('../data/word_vec_new.pkl','wb') as f:
+#     #两种效果一样
+#     ## f.write(json.dumps(all_rel_data,indent=4))
+#     # json.dump(word_vec_new,f,ensure_ascii=False)
+#     pickle.dump(word_vec_new,f)
+
+# with open('../data/word_vec_new.pkl', 'rb') as f:
+#      data = pickle.load(f)
+#      print(data)
+
 
