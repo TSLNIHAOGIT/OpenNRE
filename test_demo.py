58c9a3dc5191a5def705bef6f3d9772b59c1142f
taoshilai <taoshilai@x3platform.com>
2019-05-13 10:17:13 +0800

    update batch_size

--- test_demo.py
+++ test_demo.py
@@ -142,12 +142,12 @@ checkpoint_path=  tf.train.latest_checkpoint('./checkpoint/')
 
 if __name__=='__main__':
     # auc, pred_result = framework.test(model, ckpt="./checkpoint/" + dataset_name + "_" + model.encoder + "_" + model.selector, return_result=True)
-    auc, pred_result = framework.test(model, ckpt=checkpoint_path
-                                  , return_result=True)
-    print('auc',auc)
-
-    # pred_result=framework.predict(model, ckpt=checkpoint_path
+    # auc, pred_result = framework.test(model, ckpt=checkpoint_path
     #                               , return_result=True)
+    # print('auc',auc)
+
+    pred_result=framework.predict(model, ckpt=checkpoint_path
+                                  , return_result=True)
     # print('pred_result:')
     # for i in range(100):
     #     print(pred_result[i])
